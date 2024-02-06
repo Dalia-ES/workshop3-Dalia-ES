f292653781dbc4b22001e4473d5e553e8e899ad2
Dalia-ES <d.el-shoura@se20.qmul.ac.uk>
2024-02-06 12:43:03 +0000

    Make a for loop that loops over the numbers 10 to 20, and prints the square of each

--- Workshop 3 Part 1.R
+++ Workshop 3 Part 1.R
@@ -3,6 +3,13 @@ for(i in 1:5){ #Creates a for loop that runs 5 times (1 to 5)
 }
 x <- 0
 for(i in 1:10){
-  x <- x + i    #Adds the value of i to the value of x
+  x <- x + i #Adds the value of i to the value of x
+  print(x) #Prints each step from x+i
 }
 print(x)
+y <- 9
+for(i in 1:11){
+  y <- y+1
+  print(y)
+  print(y*y) #Prints y+1 times y+1 (the square root of each number)
+}
