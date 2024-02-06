509b05d550fcd2f63a50024ca294b60758c2b6f8
Dalia-ES <d.el-shoura@se20.qmul.ac.uk>
2024-02-06 14:04:36 +0000

    Create a loop that finds the lowest number that is a factor of 5, 6, 7 and 8

--- Workshop 3 Part 2.R
+++ Workshop 3 Part 2.R
@@ -4,3 +4,8 @@ while(x < 5){ #Creates a while loop
   print(paste('The number is now ', x, sep = ''))
 }
 
+x <- 1
+while(x %% 5 != 0 | x %% 6 != 0 | x %% 7 != 0 | x %% 8 != 0){ #The remainder of x divided by 5,6, and 7 does not equal to 0. This stops when the condition is met, printing the following
+  x <- x + 1
+}
+print(paste('The lowest number that is a factor of 5, 6, 7 and 8 is ', x, sep = ''))
