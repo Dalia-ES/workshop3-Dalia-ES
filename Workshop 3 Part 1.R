57b451f115c2d06cd12945cabe959ad46a6fcd9c
Dalia-ES <d.el-shoura@se20.qmul.ac.uk>
2024-02-06 12:27:19 +0000

    Add the value of i to the value of x

--- Workshop 3 Part 1.R
+++ Workshop 3 Part 1.R
@@ -1,3 +1,8 @@
 for(i in 1:5){ #Creates a for loop that runs 5 times (1 to 5)
   print(i) #Each time the for loop runs, print the vale of i
 }
+x <- 0
+for(i in 1:10){
+  x <- x + i    #Adds the value of i to the value of x
+}
+print(x)
